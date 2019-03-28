53c262afdc9ff83712e01d56d5ffa475ab54d87c
zhangyu <975135274@qq.com>
2019-03-28 15:16:55 +0800

    Create SpdtView.kt

---
+++ spdt-api/src/main/java/com/unionyy/mobile/spdt/skin/widget/SpdtView.kt
@@ -0,0 +1,42 @@
+package com.unionyy.mobile.spdt.skin.widget
+
+import android.content.Context
+import android.os.Build
+import android.support.annotation.RequiresApi
+import android.util.AttributeSet
+import android.view.View
+import com.unionyy.mobile.spdt.skin.widget.attrs.SpdtBackgroundHelper
+
+/**
+ * Created by 张宇 on 2019/3/25.
+ * E-mail: zhangyu4@yy.com
+ * YY: 909017428
+ */
+open class SpdtView : View {
+    constructor(context: Context) : this(context, null)
+
+    constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
+
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) :
+        super(context, attrs, defStyleAttr) {
+        init(context, attrs, defStyleAttr)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int, defStyleRes: Int) :
+        super(context, attrs, defStyleAttr, defStyleRes) {
+        init(context, attrs, defStyleAttr)
+    }
+
+    private lateinit var backgroundHelper: SpdtBackgroundHelper
+
+    private fun init(context: Context, attrs: AttributeSet?, defStyleAttr: Int) {
+        backgroundHelper = SpdtBackgroundHelper(this, attrs, defStyleAttr)
+        backgroundHelper.loadAttributes()
+    }
+
+    override fun setBackgroundResource(resid: Int) {
+        super.setBackgroundResource(resid)
+        backgroundHelper.onSetBackgroundResource(resid)
+    }
+}
