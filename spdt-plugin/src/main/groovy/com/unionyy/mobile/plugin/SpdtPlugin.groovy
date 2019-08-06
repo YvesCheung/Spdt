c70d85f07bd23908118e3db6cea63b9bc86c6099
zhangyu <975135274@qq.com>
2019-08-06 19:01:27 +0800

    update version

--- spdt-plugin/src/main/groovy/com/unionyy/mobile/plugin/SpdtPlugin.groovy
+++ spdt-plugin/src/main/groovy/com/unionyy/mobile/plugin/SpdtPlugin.groovy
@@ -47,7 +47,7 @@ class SpdtPlugin implements Plugin<Project> {
 
     private static void addDependency(Project project) {
 
-        def version = "2.0.6-SNAPSHOT"
+        def version = "2.0.7-SNAPSHOT"
 
         def getDepend = { String module ->
             def moduleProject = project.findProject(":$module")
